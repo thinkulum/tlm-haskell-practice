ea936d9d0e1aaebd9693fd74a9f5aaaf8a184035
Andy Culbertson <thinkulum@gmail.com>
2021-08-26 20:35:59 -0500

    Add concepts and the theme's initial code The new concepts are from the ch 1 section "Baby's First Functions." The practice project's theme is planning a sloth convention.

--- slothcon.hs
+++ slothcon.hs
@@ -1,8 +1,33 @@
+--- 1.3.6  Function names can't begin with capital letters.
+--- 1.3.3  Functions can call each other.
+--- 1.3.7  A function can take no parameters, in which case it's called a definition or name.
 --- 1.1.1  Haskell has arithmetic operators: +, *, -, /.
-aDifference = 4 - 5
+numAudience = numAttendees - numSpeakers
+--- 1.3.2  Functions can be defined in any order.
+numAttendees = 100
+numSpeakers = 10
 
+minSlothLengthInches = 24
+maxSlothLengthInches = 31
+
+minSlothWeightPounds = 7.9
+maxSlothWeightPounds = 17.0
+
+minThreeToedTailLengthInches = 2.0
+maxThreeToedTailLengthInches = 2.4
+
+slothWeightPoundsRoughAverage = roughAverage minSlothWeightPounds maxSlothWeightPounds
+--- 1.3.1  A function definition looks like a function call followed by = and the body.
 --- 1.1.2  Use parentheses to specify order of operations.
-aResult = (1 + 3) * 7
+roughAverage minValue maxValue = (minValue + maxValue) / 2
+
+treeBranchWeightCapacityPounds = 100
+--- 1.2.8  div executes integral division.
+--- 1.2.7  Prefix functions can be called as infix using backticks.
+--- TODO: Fix these types.
+--- treeBranchWeightCapacitySloths = treeBranchWeightCapacityPounds `div` slothWeightRoughAverage
+
+
 
 --- 1.1.3  Use parentheses for negative numbers.
 anotherDifference = 8 + (-4)
@@ -34,8 +59,6 @@ aMax = max 18739482 3
 --- 1.2.6  Function application has the highest precedence.
 anotherResult = min 8 2 * 4
 
---- 1.2.7  Prefix functions can be called as infix using backticks.
-anIntegralQuotient = 14 `div` 3
 
---- 1.2.8  div executes integral division.
-anotherIntegralQuotient = div 9 2
+--- 1.3.4  An if statement takes the form: if condition then expression else expression. The else is required.
+--- 1.3.5  Apostrophes are legal in names. They're often used to denote strict (non-lazy) or modified versions of functions.
